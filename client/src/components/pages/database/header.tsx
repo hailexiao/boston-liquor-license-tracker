b03507fac8e5eba6de1ccc1899929114ef807ba1
Haile Xiao <haile.xiao@gmail.com>
2025-06-12 16:33:34 -0400

    Move next meeting date function out of database header component

--- client/src/components/pages/database/header.tsx
+++ client/src/components/pages/database/header.tsx
@@ -4,26 +4,27 @@ import "./header.css";
 // TODO: Replace with actual logic for getting the next meeting date
 const nextMeetingDate = null;
 
+// Show the next meeting date if and only if we have one and it is in the future
+const getNextMeetingText = (nextMeeting: Date | null) => {
+  const today = new Date();
+  if (nextMeeting && nextMeeting > today) {
+    return (
+      <p className="header-text">
+        <FormattedMessage
+          id="database.header.nextMeeting"
+          values={{
+            nextMeetingDate: nextMeeting,
+            strong: (chunks: React.ReactNode) => <strong>{chunks}</strong>,
+          }}
+        />
+      </p>
+    );
+  } else {
+    return null;
+  }
+};
+
 const Header = () => {
-  // Show the next meeting date if and only if we have one and it is in the future
-  const getNextMeetingText = () => {
-    const today = new Date();
-    if (nextMeetingDate && (nextMeetingDate as Date) > today) {
-      return (
-        <p className="header-text">
-          <FormattedMessage
-            id="database.header.nextMeeting"
-            values={{
-              nextMeetingDate: nextMeetingDate,
-              strong: (chunks: React.ReactNode) => <strong>{chunks}</strong>,
-            }}
-          />
-        </p>
-      );
-    } else {
-      return null;
-    }
-  };
   return (
     <header className="database-header">
       <div className="text-container">
@@ -33,7 +34,7 @@ const Header = () => {
         <p className="header-text">
           <FormattedMessage id="database.header.description"></FormattedMessage>
         </p>
-        {getNextMeetingText()}
+        {getNextMeetingText(nextMeetingDate)}
       </div>
     </header>
   );
