1c4e64173525a6ef141a5a175481718fa06d8b26
Isaac Klar <149990794+isaac-klar@users.noreply.github.com>
2025-06-17 19:08:18 -0400

    404 Page (#104)
    Added the 404 page using the appropriate css styling and the Back to
    Home button component.
    
    ---------
    
    Co-authored-by: Matthew Clarke <4278212+MattClarke131@users.noreply.github.com>
    Co-authored-by: Dan <78275188+rann143@users.noreply.github.com>
    Co-authored-by: Filip Ilić <ilic.f@northeastern.edu>
    Co-authored-by: James Calingo <48683010+JamesCalingo@users.noreply.github.com>
    Co-authored-by: Joe <131012752+JoevaScript1@users.noreply.github.com>
    Co-authored-by: Haile Xiao <haile.xiao@gmail.com>

--- client/src/components/pages/not-found/not-found.tsx
+++ client/src/components/pages/not-found/not-found.tsx
@@ -1,12 +1,35 @@
+import notFoundStyles from "./not-found.module.css"
+import { FormattedMessage } from "react-intl";
+import beerTap from "@/assets/images/beer-tap.png";
 import BackToHome from "@/components/ui/back-to-home/back-to-home"
+
+
 
 const NotFound = () => {
   return(
-    <main className="not-found">
-      <h1>404</h1>
-      <p>Page Not Found</p>
+
+    <main className={notFoundStyles.notFound}>
+    <div className="not-found">
+      <div className="not-found-heading">
+      <h2 className="text-white text-2xl md:text-3xl lg:text-5xl w-full font-bold">
+        <FormattedMessage
+        id="notFound.heading"
+        />
+      </h2>
+      <br/>
+      <p className="text-white">
+        <FormattedMessage
+          id="notFound.message"
+        />
+      </p>
+      <br/>
+      {/* Replace with back to home component when done */}
       <BackToHome />
+      </div>
+    </div>
+      <img src={beerTap} className="beer-tap" alt="Beer Tap"/>
     </main>
+
   )
 }
 
