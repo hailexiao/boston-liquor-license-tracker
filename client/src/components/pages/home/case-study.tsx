09e68b49a32fb564d0432056a16588847c6f6100
Ro Desai <desai.ro@gmail.com>
2025-06-24 00:34:32 -0400

    Case study Component Fixes - formatted message for translations, CSS Modules, nesting CSS (#114)
    Addressed and resolved previously given feedback from original Case
    Study component work:
    --CSS syntax
    --Removing button functionality
    --The <br> tag
    --Translations
    Changes related to CSS Modules approach including:
    --Updated custom CSS classname for CSS Modules
    --Updated div class name syntax for CSS Modules
    --Cleaned up some mixed/redundant use of standard CSS with Tailwind CSS
    Also resolved merge conflicts while rebasing my feature branch with
    latest codebase from main repo

--- client/src/components/pages/home/case-study.tsx
+++ client/src/components/pages/home/case-study.tsx
@@ -1,10 +1,78 @@
 import "./case-study.module.css";
+import { useIntl, FormattedMessage } from 'react-intl';
+import myImage from '/src/assets/images/phimgsq460.png';
+import caseStudyStyles from "./case-study.module.css";
 
 const CaseStudy = () => {
+  const intl = useIntl();
   return (
-    <div className="case-study">
-      <h1>Case Study</h1>
-    </div>
+    <div className={caseStudyStyles.caseStudy}>
+        <div className="!p-0 !m-0 max-w-[840px] w-auto flex-shrink">
+          <h1 className ="text-2xl max-w-[840px] w-auto flex-shrink mb-[16px]">
+            <FormattedMessage
+              id="caseStudy.title"
+            />
+          </h1>
+          <h2 className="case-study h2 !p-0 !m-0 !ml-0 !pl-0 !indent-0 ![margin-bottom:32px]">
+            <FormattedMessage
+              id="caseStudy.par1"
+            />
+          <br />
+          <br />
+          <FormattedMessage
+              id="caseStudy.par2"
+            />
+          <br />
+          <br />
+          <FormattedMessage
+              id="caseStudy.par3"
+          />
+          </h2>
+        </div>
+        <div className={`${caseStudyStyles.dottedThickBorder} flex justify-center h-auto overflow-hidden mx-auto max-w-[480px] box-border ![margin-bottom:32px]`}
+          >
+          <img
+            src={myImage}
+            alt={intl.formatMessage({ id: "caseStudy.image.alt" })}
+            className="
+              w-full
+              max-w-[480px]
+              h-auto
+              object-cover
+              transition-transform duration-300 hover:scale-105
+              block
+              m-auto
+            "
+          />
+        </div>
+        <div className="
+          w-full
+          flex
+          justify-center
+          ![margin-bottom:64px]
+          "
+          >
+          <button
+            className="
+              inline-flex
+              items-center
+              justify-center
+              hover:bg-black
+              text-black
+              !bg-[#F5B524]
+              font-bold
+              px-8
+              py-4
+              rounded-lg shadow-md
+              gap-2.5
+              "
+            >
+            <FormattedMessage
+              id="caseStudy.button.read"
+            />
+          </button>
+        </div>
+      </div>
   );
 };
 
--- client/src/components/pages/home/extra-work.tsx
+++ client/src/components/pages/home/extra-work.tsx
@@ -3,7 +3,9 @@ import "./extra-work.module.css";
 const ExtraWork = () => {
   return (
     <div className="extra-work">
-      <h1>Extra Work</h1>
+      <div className="w-full">
+        <h1>Extra Work</h1>
+      </div>
     </div>
   );
 };
