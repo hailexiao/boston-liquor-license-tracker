de06472a95aefe05a288e2593ad9872ffb0c7d80
Haile Xiao <haile.xiao@gmail.com>
2025-08-19 19:08:08 -0400

    Add translatable alt text to images

--- client/src/components/pages/coming-soon/coming-soon.tsx
+++ client/src/components/pages/coming-soon/coming-soon.tsx
@@ -2,10 +2,15 @@ import PageHeader from "@components/pages/coming-soon/pageheader.tsx";
 import BackToHome from "@components/ui/back-to-home.tsx";
 import { FormattedMessage } from "react-intl";
 import styles from "./coming-soon.module.css";
+import { useIntl } from "react-intl";
 
 const ComingSoon = () => {
+  const intl = useIntl();
   return (
-    <main className={`${styles.comingSoonPage} coming-soon-page`}>
+    <main
+      className={`${styles.comingSoonPage} coming-soon-page`}
+      title={intl.formatMessage({ id: "comingSoon.backgroundImageAlt" })}
+    >
       <PageHeader
         headerTitle={<FormattedMessage id="comingSoon.title" />}
         headerText={<FormattedMessage id="comingSoon.description" />}
