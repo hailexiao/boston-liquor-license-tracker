46879fe8a761c2eec09ddf40359e3068b9cd79c2
Haile Xiao <haile.xiao@gmail.com>
2025-09-02 23:15:56 -0400

    Move initial HTML lang attribute setting to main.tsx

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -2,9 +2,6 @@ import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import NotFound from "@/components/pages/not-found/not-found";
-import { K_LOCALE } from "@/i18n/stored-locale";
-
-const locale = localStorage.getItem(K_LOCALE) || "en-US";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -12,7 +9,6 @@ export const Route = createRootRoute({
 });
 
 export function RootComponent() {
-  document.documentElement.lang = locale;
   return (
     <>
       <HeadContent />
